db3ae8d1ba864c598785b9e3736c86b392654220
fil2k <cavalheirofilipe@gmail.com>
2023-08-25 15:45:40 +0100

    made dijkstra and aStar algorithems

--- aStar.c
+++ aStar.c
@@ -1,21 +1,21 @@
-#include "dijkstra.h"
+#include "aStar.h"
 
-struct _dijkstraNode{
+struct _aStarNode{
     int coords[2];
-    int value;
+    float value;
     int parent[2];
 };
 
-int getElem(dijkstraNode elem){
+int getElem(aStarNode elem){
     return elem->value;
 }
 
 int getElemGen(void* elem){
-    return getElem((dijkstraNode)elem);
+    return getElem((aStarNode)elem);
 }
 
-dijkstraNode makeDijkstraNode(int* coords, int value, int* parent){
-    dijkstraNode elem = (dijkstraNode)malloc(sizeof(struct _dijkstraNode));
+aStarNode makeDijkstraNode(int* coords, float value, int* parent){
+    aStarNode elem = (aStarNode)malloc(sizeof(struct _aStarNode));
     if(elem == NULL){
         perror("could not create dijkstra Node\n");
         return NULL;
@@ -40,7 +40,7 @@ linkedList closest_path(int matrix[][6], hashtable ht,int* key, int* startPos){
     linkedList path = makeLinkedList();
     int* tmp;
     while((key[0] != startPos[0]) || (key[1] != startPos[1])){
-        tmp = ((dijkstraNode)getElem_ht(ht, key))->parent;
+        tmp = ((aStarNode)getElem_ht(ht, key))->parent;
         key[0] = tmp[0];
         key[1] = tmp[1];
         matrix[key[0]][key[1]] = 5;
@@ -49,20 +49,24 @@ linkedList closest_path(int matrix[][6], hashtable ht,int* key, int* startPos){
     return path;
 }
 
+float distance(int pointA[2], int pointB[2]){
+    return sqrt((((float)pointA[0] + (float)pointB[0])*((float)pointA[0] + (float)pointB[0])) + (((float)pointA[1] + (float)pointB[1]) * ((float)pointA[1] + (float)pointB[1])));
+}
+
 linkedList dijkstra(int startPos[2], int finishPos[2], int matrix[][6], int* costMatrix){
     hashtable table = makeHashtable();
-    dijkstraNode elem = makeDijkstraNode(startPos, 0, startPos);
-    printf("created a Node: {%d, %d}, %d\n", elem->coords[0], elem->coords[1], elem->value);
+    aStarNode elem = makeDijkstraNode(startPos, 0, startPos);
+    printf("created a Node: {%d, %d}, %f\n", elem->coords[0], elem->coords[1], elem->value);
     insert_ht(table, elem->coords, elem);
     binaryTree tree = makeBinaryTree(elem);
 
     int* pos;
-    dijkstraNode currentNode;
-    dijkstraNode oldNode;
+    aStarNode currentNode;
+    aStarNode oldNode;
     int count = 0;
     while(tree != NULL){
-        currentNode = (dijkstraNode)getElem_bt(removeMinNode(&tree));
-        printf("Removed a Node: {%d, %d}, %d\n", currentNode->coords[0], currentNode->coords[1], currentNode->value);
+        currentNode = (aStarNode)getElem_bt(removeMinNode(&tree));
+        printf("Removed a Node: {%d, %d}, %f\n", currentNode->coords[0], currentNode->coords[1], currentNode->value);
         if((currentNode->coords[0] == finishPos[0]) && (currentNode->coords[1] == finishPos[1])){
             linkedList path = closest_path(matrix, table, currentNode->coords, startPos);
             matrix[startPos[0]][startPos[1]] = 2;
@@ -71,7 +75,7 @@ linkedList dijkstra(int startPos[2], int finishPos[2], int matrix[][6], int* cos
         }
         matrix[(currentNode->coords)[0]][(currentNode->coords)[1]] = 4; 
         pos = currentNode->coords;
-        oldNode = (dijkstraNode)getElem_ht(table, pos);
+        oldNode = (aStarNode)getElem_ht(table, pos);
         if(oldNode != NULL){
             if(oldNode->value < currentNode->value)
                 continue;
@@ -81,20 +85,21 @@ linkedList dijkstra(int startPos[2], int finishPos[2], int matrix[][6], int* cos
                 int chekingPos[2] = {pos[0] + i, pos[1] + j};
                 int typePos = checkPos(chekingPos, matrix);
                 if((typePos != 0) && (typePos != 3)){continue;}        
-                oldNode = (dijkstraNode)getElem_ht(table, chekingPos);
+                oldNode = (aStarNode)getElem_ht(table, chekingPos);
                 if(oldNode != NULL){
                     if(oldNode->value > currentNode->value + 1){
                         oldNode->value = currentNode->value + 1;
                         oldNode->parent[0] = currentNode->coords[0];
                         oldNode->parent[1] = currentNode->coords[1];
                         tree = insert_bt(tree, oldNode, getElemGen);
-                        printf("changed a Node: {%d, %d}, %d\n", oldNode->coords[0], oldNode->coords[1], oldNode->value);
+                        printf("changed a Node: {%d, %d}, %f\n", oldNode->coords[0], oldNode->coords[1], oldNode->value);
                     }
                     continue;
                 }
-                dijkstraNode newNode = makeDijkstraNode(chekingPos, currentNode->value + costMatrix[count], currentNode->coords);
+                float cost = currentNode->value + costMatrix[count] + distance(currentNode->coords, finishPos);
+                aStarNode newNode = makeDijkstraNode(chekingPos, cost, currentNode->coords);
                 ++count;
-                printf("created a Node: {%d, %d}, %d\n", newNode->coords[0], newNode->coords[1], newNode->value);
+                printf("created a Node: {%d, %d}, %f\n", newNode->coords[0], newNode->coords[1], newNode->value);
                 insert_ht(table, newNode->coords, newNode);
                 tree = insert_bt(tree, newNode, getElemGen);
                 continue;
--- aStar.h
+++ aStar.h
@@ -1,21 +1,24 @@
-#ifndef DIJKSTRA_H
-#define DIJKSTRA_H
+#ifndef ASTAR_H
+#define ASTAR_H
 
 #include "../Data-structures-in-C/hashtable.h"
 #include "../Data-structures-in-C/binaryTree.h"
 #include "../Data-structures-in-C/linkedList.h"
-typedef struct _dijkstraNode *dijkstraNode;
-#include "dijkstra.c"
+#include <stdio.h>
+typedef struct _aStarNode *aStarNode;
+#include "aStar.c"
 
-int getElem(dijkstraNode elem);
+int getElem(aStarNode elem);
 
 int getElemGen(void* elem);
 
-dijkstraNode makeDijkstraNode(int* coords, int value, int* parent);
+aStarNode makeDijkstraNode(int* coords, float value, int* parent);
 
 int checkPos(int* position, int matrix[][6]);
 
 linkedList closest_path(int matrix[][6], hashtable ht,int* key, int* startPos);
 
+float distance(int pointA[2], int pointB[2]);
+
 linkedList dijkstra(int startPos[2], int finishPos[2], int matrix[][6], int* costMatrix);
-#endif // DIJKSTRA_H
+#endif // ASTAR_H
